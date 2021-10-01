4a961bbe8f876cb1a560c0df1431721d92835c6b
Thomas <ziahyonchume@gmail.com>
2021-09-30 22:43:08 -0400

    corrected slideshow

--- static/js/js.js
+++ static/js/js.js
@@ -13,13 +13,9 @@ var slideArray = [];
 var index_i = 0;
 var time = 2400;
 
-var ss_img1 = "{% static 'img/slide1.jpg'%}";
-var ss_img2 = "{% static 'img/slide2.jpg'%}"; 
-var ss_img3 = "{% static 'img/slide3.jpg'%}"; 
-
-slideArray[0] = ss_img1; // from: https://pixabay.com/photos/money-donation-donate-profit-give-652560/
-slideArray[1] = ss_img2; // from: https://pixabay.com/photos/donate-control-money-support-civil-5281984/
-slideArray[2] = ss_img3; // from: https://pixabay.com/photos/hand-hold-reach-place-846092/
+slideArray[0] = "static/img/slide1.jpg"; // from: https://pixabay.com/photos/money-donation-donate-profit-give-652560/
+slideArray[1] = "static/img/slide2.jpg"; // from: https://pixabay.com/photos/donate-control-money-support-civil-5281984/
+slideArray[2] = "static/img/slide3.jpg"; // from: https://pixabay.com/photos/hand-hold-reach-place-846092/
 
 function set_slideshow() {
     slideshow.setAttribute("src", slideArray[index_i]);

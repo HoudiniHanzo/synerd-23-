20341a7673912381f1dd511aa2e1eebfd024b18a
Thomas <ziahyonchume@gmail.com>
2021-09-30 22:40:03 -0400

    attempt 2 for slideshow

--- static/js/js.js
+++ static/js/js.js
@@ -13,6 +13,10 @@ var slideArray = [];
 var index_i = 0;
 var time = 2400;
 
+var ss_img1 = "{% static 'img/slide1.jpg'%}";
+var ss_img2 = "{% static 'img/slide2.jpg'%}"; 
+var ss_img3 = "{% static 'img/slide3.jpg'%}"; 
+
 slideArray[0] = ss_img1; // from: https://pixabay.com/photos/money-donation-donate-profit-give-652560/
 slideArray[1] = ss_img2; // from: https://pixabay.com/photos/donate-control-money-support-civil-5281984/
 slideArray[2] = ss_img3; // from: https://pixabay.com/photos/hand-hold-reach-place-846092/

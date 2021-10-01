84d22baf25d6bf243163a440b4e60e63224e3860
Thomas <ziahyonchume@gmail.com>
2021-09-30 23:50:15 -0400

    archiving older versions

---
+++ older version/build_9_30_21/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('', views.a_index, name='home'),
+    path('index/', views.a_index, name='index'),
+    path('register/', views.a_register, name='register'),
+    path('login/', views.a_login, name='login'),
+    path('dashboard/', views.a_dashboard, name='dashboard'),
+]
+
+urlpatterns += staticfiles_urlpatterns()

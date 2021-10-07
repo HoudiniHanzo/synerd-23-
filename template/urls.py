b0397ddccf0a87d1ad9b6dd1a337d6d3ecac3165
Thomas <ziahyonchume@gmail.com>
2021-10-07 17:33:16 -0400

    removing index path

--- template/urls.py
+++ template/urls.py
@@ -5,7 +5,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.a_index, name='home'),
-    path('index/', views.a_index, name='home'),
     path('register/', views.a_register, name='register'),
     path('login/', views.a_login, name='login'),
     path('dashboard/', views.a_dashboard, name='dashboard'),

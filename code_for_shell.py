fa507f6e54a9f5330248db48a13dc67d4d46d643
Thomas <ziahyonchume@gmail.com>
2021-10-19 22:44:00 -0400

    Attempted fkeys. Too many errors. Reverted back.

--- code_for_shell.py
+++ code_for_shell.py
@@ -13,27 +13,16 @@ service1.delete()
 # Simple Insertion
 # Requires the primary key or else will not function correctly.
 # #
-Andy = Subscriber(subscriberID='1001', requestcode='1001', startdate='2015-04-12', enddate=None, motifofcancellation='', beneficiaryID='85006341')
+Andy = Subscriber(subscriberID='1001', requestcode='1001', startdate='2015-04-12', enddate=None,beneficiaryID='85006341')
 Bree = Subscriber(subscriberID='1002', requestcode='2003', startdate='2011-07-22', motifofcancellation='', beneficiaryID='90045577')
 Cooral = Subscriber(subscriberID='3456', requestcode='8009', startdate='2016-09-06', enddate='2020-08-31', motifofcancellation='Did not carry the 1.')
 
-# Other Updating
+# Updating Data
 # Updating Subscribers servicecode for current services.
 # #
-Andy.servicecode = '339988'
-Bree.servicecode = '339988'
+Andy.servicecode = str(service2)
+Bree.servicecode = str(service2)
 Andy.save()
 Bree.save()
 Cooral.save()
-
-# Updating Data
-# Updates transfer1's columns to 'Place A' and 'Place B' to 'Place E' and 'Place F'
-# #
-transfer1 = TransferredSubscription(transferID='1212', transfer_from='Place A', transfer_to='Place B', request_date='2005-5-16', transfer_date='2006-6-16', subscriberID=str(Andy))
-transfer2 = TransferredSubscription(transferID='1313', transfer_from='Place C', transfer_to='Place D', request_date='2006-6-17', transfer_date='2006-8-18', subscriberID=str(Cooral))
-transfer1.save()
-transfer2.save()
-transfer1.transfer_from = 'Place E'
-transfer1.transfer_to = 'Place F'
-transfer1.save()
 
